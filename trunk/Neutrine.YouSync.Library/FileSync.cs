dbfd5ad159f0bdf38cabcc28175fa57f3117fa18
lpicanco <lpicanco@447ef688-e8f5-11dd-97d9-e7a3ff7b18fb>
2009-01-23 02:35:36 +0000

    Import Inicial

---
+++ trunk/Neutrine.YouSync.Library/FileSync.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+using Microsoft.Synchronization.Data;
+
+namespace Neutrine.YouSync.Library
+{
+    public class FileSync
+    {
+        public void DetectChangesOnFileSystemReplica(
+                SyncId replicaId, string replicaRootPath,
+                FileSyncScopeFilter filter, FileSyncOptions options)
+        {
+            FileSyncProvider provider = null;
+
+            try
+            {
+                provider = new FileSyncProvider(replicaId.GetGuidId(), replicaRootPath, filter, options);
+                provider.DetectChanges();
+            }
+            finally
+            {
+                // Release resources
+                if (provider != null)
+                    provider.Dispose();
+            }
+        }
+
+        public void SyncFileSystemReplicasOneWay(
+                SyncId sourceReplicaId, SyncId destinationReplicaId,
+                string sourceReplicaRootPath, string destinationReplicaRootPath,
+                FileSyncScopeFilter filter, FileSyncOptions options)
+        {
+            FileSyncProvider sourceProvider = null;
+            FileSyncProvider destinationProvider = null;
+
+            try
+            {
+                sourceProvider = new FileSyncProvider(
+                    sourceReplicaId.GetGuidId(), sourceReplicaRootPath, filter, options);
+                destinationProvider = new FileSyncProvider(
+                    destinationReplicaId.GetGuidId(), destinationReplicaRootPath, filter, options);
+
+                destinationProvider.AppliedChange +=
+                    new EventHandler<AppliedChangeEventArgs>(OnAppliedChange);
+                destinationProvider.SkippedChange +=
+                    new EventHandler<SkippedChangeEventArgs>(OnSkippedChange);
+
+                SyncAgent agent = new SyncAgent();
+                agent.LocalProvider = sourceProvider;
+                agent.RemoteProvider = destinationProvider;
+                agent.Direction = SyncDirection.Upload; // Sync source to destination
+
+                Console.WriteLine("Synchronizing changes to replica: " +
+                    destinationProvider.RootDirectoryPath);
+                agent.Synchronize();
+            }
+            finally
+            {
+                // Release resources
+                if (sourceProvider != null) sourceProvider.Dispose();
+                if (destinationProvider != null) destinationProvider.Dispose();
+            }
+        }
+
+        public void OnAppliedChange(object sender, AppliedChangeEventArgs args)
+        {
+            switch (args.ChangeType)
+            {
+                case ChangeType.Create:
+                    Console.WriteLine("-- Applied CREATE for file " + args.NewFilePath);
+                    break;
+                case ChangeType.Delete:
+                    Console.WriteLine("-- Applied DELETE for file " + args.OldFilePath);
+                    break;
+                case ChangeType.Overwrite:
+                    Console.WriteLine("-- Applied OVERWRITE for file " + args.OldFilePath);
+                    break;
+                case ChangeType.Rename:
+                    Console.WriteLine("-- Applied RENAME for file " + args.OldFilePath +
+                                      " as " + args.NewFilePath);
+                    break;
+            }
+        }
+
+        public void OnSkippedChange(object sender, SkippedChangeEventArgs args)
+        {
+            Console.WriteLine("-- Skipped applying " + args.ChangeType.ToString().ToUpper()
+                  + " for " + (!string.IsNullOrEmpty(args.CurrentFilePath) ?
+                                args.CurrentFilePath : args.NewFilePath) + " due to error");
+
+            if (args.Exception != null)
+                Console.WriteLine("   [" + args.Exception.Message + "]");
+        }
+
+        public SyncId GetReplicaId(string idFilePath)
+        {
+            SyncId replicaId = null;
+
+            if (File.Exists(idFilePath))
+            {
+                using (StreamReader sr = File.OpenText(idFilePath))
+                {
+                    string strGuid = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(strGuid))
+                        replicaId = new SyncId(new Guid(strGuid));
+                }
+            }
+
+            if (replicaId == null)
+            {
+                using (FileStream idFile = File.Open(
+                            idFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter sw = new StreamWriter(idFile))
+                    {
+                        replicaId = new SyncId(Guid.NewGuid());
+                        sw.WriteLine(replicaId.GetGuidId().ToString("D"));
+                    }
+                }
+            }
+
+            return replicaId;
+        }
+    }
+}
---
+++ trunk/Neutrine.YouSync.Tests/UnitTest1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Microsoft.Synchronization;
+using Microsoft.Synchronization.Files;
+using System.IO;
+
+namespace Neutrine.YouSync.Tests
+{
+    /// <summary>
+    /// Summary description for UnitTest1
+    /// </summary>
+    [TestClass]
+    public class UnitTest1
+    {
+        public UnitTest1()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            String idFileName = ".yousync";
+
+            String sourcePath = @"C:\temp\app\app";
+            String destPath = @"C:\temp\sync";
+            Guid sourceId = GetReplicaId(Path.Combine(sourcePath, idFileName));
+            Guid destId = GetReplicaId(Path.Combine(destPath, idFileName));
+            
+            FileSyncProvider sourceProvider = new FileSyncProvider(sourceId, sourcePath);
+            FileSyncProvider destProvider = new FileSyncProvider(destId, destPath);
+
+            sourceProvider.
+
+            SyncOrchestrator sync = new SyncOrchestrator();
+            sync.LocalProvider = sourceProvider;
+            sync.RemoteProvider = destProvider;
+            sync.Direction = SyncDirectionOrder.UploadAndDownload;
+            sync.Synchronize();
+        }
+
+
+        public Guid GetReplicaId(string idFilePath)
+        {
+            Guid replicaId = Guid.Empty;
+
+            if (File.Exists(idFilePath))
+            {
+                using (StreamReader sr = File.OpenText(idFilePath))
+                {
+                    string strGuid = sr.ReadLine();
+                    if (!string.IsNullOrEmpty(strGuid))
+                        replicaId = new Guid(strGuid);
+                }
+            }
+
+            if (replicaId == Guid.Empty)
+            {
+                using (FileStream idFile = File.Open(
+                            idFilePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+                {
+                    using (StreamWriter sw = new StreamWriter(idFile))
+                    {
+                        replicaId = Guid.NewGuid();
+                        sw.WriteLine(replicaId.ToString("D"));
+                    }
+                }
+            }
+
+            return replicaId;
+        }
+    }
+}
